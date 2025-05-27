816eb4b8431266411cb14f125f03c2512c18a647
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2025-05-27 15:37:28 +0200

    Update read_pickle.py

--- src/utils/read_pickle.py
+++ src/utils/read_pickle.py
@@ -1,7 +1,7 @@
 import pickle
 import os
 # Replace with your pickle file path
-pickle_file = '/predictions/depth_new.pkl'
+pickle_file = 'predictions/depth_new.pkl'
 path = os.path.join(os.getcwd(), pickle_file)
 print(f"reading pickle file in path: {path}")
 with open(path, 'rb') as f:
