8a8d37f45a8613fec771f2454f011b0f709c203c
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2025-05-28 09:19:06 +0200

    Update test_baracca.py

--- src/test_baracca.py
+++ src/test_baracca.py
@@ -192,6 +192,7 @@ class PoseTest(object):
                 # Saving images
                 if self.configer.get("save_img") is True:
                     name = self.configer.get("data", "type")
+                    print(f"kpts_pred shape: {kpts_pred.shape}")
                     self.save_images(ids, kpts_pred, kpts_in, visible, f"predictions/imgs/{name}")
                     self.img_saved += ids.shape[0]
 
